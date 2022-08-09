970b6381fb51148749d63bd54db32dcf43b15ef4
Stierle <paul.stierle@pf2542k6dk.fritz.box>
2022-08-09 11:13:19 +0200

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: "https://mdshzzglpsfqicxqoggy.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kc2h6emdscHNmcWljeHFvZ2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAwMzU5NTYsImV4cCI6MTk3NTYxMTk1Nn0.aSn2ojiZWAI3DvlweiEY68X0Hk8t94ps3TaEGDtva2U"
 };
 
 /*
